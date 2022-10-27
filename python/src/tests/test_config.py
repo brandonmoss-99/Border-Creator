4ff6873caca8afe58cc07f3fe582dc66752ef38b
Brandon Moss <88378432+brandonmoss-99@users.noreply.github.com>
2022-10-27 22:20:00 +0100

    Add more tests

---
+++ python/src/tests/test_config.py
@@ -0,0 +1,67 @@
+import unittest
+import config
+
+class Test_createConfig(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_filePath(self):
+        conf = {"file": "test.jpg", "border": 10, "colour": "black", "useLong": True}
+        parsedConf = config.Config(conf)
+
+        self.assertEqual(parsedConf.dirPath, None)
+        self.assertEqual(parsedConf.filePath, "test.jpg")
+    
+
+    def test_dirPath(self):
+        conf = {"dir": "test", "border": 10, "colour": "black", "useLong": True}
+        parsedConf = config.Config(conf)
+        
+        self.assertEqual(parsedConf.dirPath, "test")
+        self.assertEqual(parsedConf.filePath, None)
+
+
+    def test_borderAmount(self):
+        conf = {"file": "test.jpg", "border": 10, "colour": "black", "useLong": True}
+        parsedConf = config.Config(conf)
+
+        self.assertEqual(parsedConf.borderAmount, 10)
+    
+
+    def test_default_borderAmount(self):
+        conf = {"file": "test.jpg", "colour": "black", "useLong": True}
+        parsedConf = config.Config(conf)
+
+        self.assertEqual(parsedConf.borderAmount, 5)
+
+
+    def test_colour(self):
+        conf = {"file": "test.jpg", "border": 10, "colour": "black", "useLong": True}
+        parsedConf = config.Config(conf)
+
+        self.assertEqual(parsedConf.colour, "black")
+
+
+    def test_default_colour(self):
+        conf = {"file": "test.jpg", "border": 10, "useLong": True}
+        parsedConf = config.Config(conf)
+
+        self.assertEqual(parsedConf.colour, "white")
+
+
+    def test_useLong(self):
+        conf = {"file": "test.jpg", "border": 10, "colour": "black", "useLong": True}
+        parsedConf = config.Config(conf)
+
+        self.assertEqual(parsedConf.useLong, True)
+
+
+    def test_default_useLong(self):
+        conf = {"file": "test.jpg", "border": 10, "colour": "black"}
+        parsedConf = config.Config(conf)
+
+        self.assertEqual(parsedConf.useLong, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- python/src/tests/test_processor.py
+++ python/src/tests/test_processor.py
@@ -6,18 +6,20 @@ class Test_borderCalculator(unittest.TestCase):
     def setUp(self):
         pass
     
+
     def test_BorderSizeCalculation(self):
         c_1 = Config({"file": "test.jpg", "border": 10, "useLong": False})
-        self.assertEqual(processor.calculateBorderSize(c_1, 4000, 3000), 300)
-
         c_2 = Config({"file": "test.jpg", "border": 10, "useLong": False})
+
+        self.assertEqual(processor.calculateBorderSize(c_1, 4000, 3000), 300)
         self.assertEqual(processor.calculateBorderSize(c_2, 3000, 4000), 300)
+
 
     def test_longBorderSizeCalculation(self):
         c_1 = Config({"file": "test.jpg", "border": 10, "useLong": True})
-        self.assertEqual(processor.calculateBorderSize(c_1, 4000, 3000), 400)
-
         c_2 = Config({"file": "test.jpg", "border": 10, "useLong": True})
+
+        self.assertEqual(processor.calculateBorderSize(c_1, 4000, 3000), 400)
         self.assertEqual(processor.calculateBorderSize(c_2, 3000, 4000), 400)
 
 
@@ -25,18 +27,22 @@ class Test_extensionParser(unittest.TestCase):
     def setUp(self):
         pass
 
+
     def test_jpg(self):
         f = "test.jpg"
         self.assertEqual(processor.getExtension(f), "jpg")
     
+
     def test_multidotFile(self):
         f = "test.file.jpg"
         self.assertEqual(processor.getExtension(f), "jpg")
+
 
     def test_manyMultidotFile(self):
         f = "test.file.with.many.dots.png.jpg.tiff.jpg.png"
         self.assertEqual(processor.getExtension(f), "png")
     
+
     def test_noExtension(self):
         f = "directory"
         self.assertEqual(processor.getExtension(f), "")
